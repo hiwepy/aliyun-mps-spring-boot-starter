f1af91a058b22a04960edf5db0b9cf0b03c3b3e9
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:14:49 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.4.x branch

--- src/main/java/com/aliyun/mps/spring/boot/model/TransformComplexRequest.java
+++ src/main/java/com/aliyun/mps/spring/boot/model/TransformComplexRequest.java
@@ -26,7 +26,7 @@ import lombok.Data;
 
 /**
  * 复杂的媒体处理参数对象（支持格式转换、拼接、剪辑）
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @JsonInclude( JsonInclude.Include.NON_NULL)
 @Data
