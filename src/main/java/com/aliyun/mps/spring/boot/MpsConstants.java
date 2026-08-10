32a094a1bd3f347588d5c510a8642d848d35bbdc
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:17:44 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/com/aliyun/mps/spring/boot/MpsConstants.java
+++ src/main/java/com/aliyun/mps/spring/boot/MpsConstants.java
@@ -17,7 +17,7 @@ package com.aliyun.mps.spring.boot;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public final class MpsConstants {
 
