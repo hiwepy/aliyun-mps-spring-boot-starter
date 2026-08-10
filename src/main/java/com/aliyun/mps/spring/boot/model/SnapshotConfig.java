9c6fe3c21feb99a8bca44ce70b70f739acb82f18
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:17:40 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/com/aliyun/mps/spring/boot/model/SnapshotConfig.java
+++ src/main/java/com/aliyun/mps/spring/boot/model/SnapshotConfig.java
@@ -25,7 +25,7 @@ import lombok.Data;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
